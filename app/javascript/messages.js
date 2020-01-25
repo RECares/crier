73e339226a748af2b95651713cced081a42c3daa
Jared Payne <jaredmpayne@gmail.com>
2020-01-24 23:56:32 -0500

    Add Redis gem to production
    Redis allows ActionCable to run properly in a production environment.

--- app/javascript/messages.js
+++ app/javascript/messages.js
@@ -3,8 +3,10 @@ import consumer from './channels/consumer'
 $(document).on("click", ".send-comment-button", (event) => {
     var commentForm = $(event.currentTarget).closest(".comment-form");
     var textarea = commentForm.find(".message-textarea");
-    consumer.messages.speak(textarea.val());
-    textarea.val("");
+    if (textarea.val()) {
+        consumer.messages.speak(textarea.val());
+        textarea.val("");
+    }
 });
 
 $(document).on("click", ".reply-link", (event) => {
@@ -25,7 +27,9 @@ $(document).on("click", ".send-reply-button", (event) => {
     var message = $(event.currentTarget).closest(".message");
     var replyForm = message.children(".reply-form");
     var textarea = replyForm.find(".reply-textarea");
-    consumer.messages.speak(textarea.val(), message.attr("id"));
-    replyForm.hide();
-    textarea.val("");
+    if (textarea.val()) {
+        consumer.messages.speak(textarea.val(), message.attr("id"));
+        replyForm.hide();
+        textarea.val("");
+    }
 });
